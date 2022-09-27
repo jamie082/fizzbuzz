d70dc81a55559daf88c3266faaa54fd60a9841e6
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-09-27 04:37:29 +0000

    added working source

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fizz_buzz():
+    print("FizzBuzz program")
+    i = input()
+
+    if (i % 3 and i % 5)
+        print("FizzBuzz")
+    elif (i % 3)
+        print("Fizz")
+    elif (i % 5)
+        print("Buzz")
+    else:
+
+
+fizz_buzz()
