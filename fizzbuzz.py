4a6dec58f8a370694bd2cbfdddd007329a12ba2d
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-09-30 03:12:10 +0000

    added source

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,16 +3,13 @@
 def fizz_buzz():
     n = int(input("Input number for fizzbuzz: "))
 
-    for d in range(1, n):
-        if (n % 3 and n % 5):
-            print(str(d) + "FizzBuzz", end=' ')
-        elif (n % 3): # string.add
-            #print(str(d) + "Fizz", end=' ')
-            print(str(d) + d.replace(d, "Fizz"))
+    for d in range(1, n+1):
+        if ((n % 3 and n % 5)):
+            print(str(d), + d.replace(n, "FizzBuzz", end=' ')
+        elif (n % 3): 
+            print(str(d), + d.replace(n, "Fizz"), end=' ')
         elif (n % 5):
-            print(str(d) + "Buzz", end=' ')
-        else:
-            print("You didn't enter a correct value" + i + "and the end")
+            print(str(d), + d.replace(n, "Buzz"), end=' ')
 
 fizz_buzz()
 
