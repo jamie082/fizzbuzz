7b902634417084799bf64a919ebdabe74e9b32c4
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-09-30 05:19:05 +0000

    added source

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
+import pdb
+
+fizzbuzz = "FizzBuzz"
+buzz = "Buzz"
+fizz = "Fizz"
+
 def fizz_buzz():
     n = int(input("Input number for fizzbuzz: "))
 
     for d in range(1, n+1):
-        if ((n % 3 and n % 5)):
-            print(str(d), + d.replace(n, "FizzBuzz", end=' ')
+        if (n % 3 and n % 5):
+            print(d, end=" ")
         elif (n % 3): 
-            print(str(d), + d.replace(n, "Fizz"), end=' ')
+            print(d, end=" ")
         elif (n % 5):
-            print(str(d), + d.replace(n, "Buzz"), end=' ')
+            print(d, end=" ")
+        else:
+            print("Error! No match found!")
 
 fizz_buzz()
-
