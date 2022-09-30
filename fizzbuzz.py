3e124ccb1431bb8616803c0e8e12ce1ea837d6c3
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-09-30 07:03:04 +0000

    added working code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,16 +7,12 @@ buzz = "Buzz"
 fizz = "Fizz"
 
 def fizz_buzz():
-    n = int(input("Input number for fizzbuzz: "))
+    for d in range(51):
+        if (d % 3 == 0 and d % 5 == 0):
+            print(fizzbuzz, end=" ")
+        elif (d % 3 == 0): 
+            print(fizz, end=" ")
+        elif (d % 5 == 0):
+            print(buzz, end=" ")
 
-    for d in range(1, n+1):
-        if (n % 3 and n % 5):
-            print(d, end=" ")
-        elif (n % 3): 
-            print(d, end=" ")
-        elif (n % 5):
-            print(d, end=" ")
-        else:
-            print("Error! No match found!")
-
-fizz_buzz()
+        print(d)
