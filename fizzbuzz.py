5bfa98ef296751bbf149ec1df73088da46b5bf96
Jamie Morrissey <jamiemorrissey02@gmail.com>
2022-09-30 07:05:11 +0000

    added working source

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,13 +6,15 @@ fizzbuzz = "FizzBuzz"
 buzz = "Buzz"
 fizz = "Fizz"
 
-def fizz_buzz():
-    for d in range(51):
-        if (d % 3 == 0 and d % 5 == 0):
-            print(fizzbuzz, end=" ")
-        elif (d % 3 == 0): 
-            print(fizz, end=" ")
-        elif (d % 5 == 0):
-            print(buzz, end=" ")
+for d in range(51):
+    if (d % 3 == 0 and d % 5 == 0):
+         print(fizzbuzz, end=" ")
+         continue
+    elif (d % 3 == 0): 
+         print(fizz, end=" ")
+         continue
+    elif (d % 5 == 0):
+         print(buzz, end=" ")
+         continue
 
-        print(d)
+         print(d)
